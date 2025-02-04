881c388b4e60382715da08da1a937bd4681616ce
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-02-04 23:03:09 +0600

    data type practice done'

---
+++ variable.js
@@ -0,0 +1,14 @@
+var name = "Fahad Ahmad";
+console.log(name);
+
+var age = 24;
+console.log(age);
+
+var pass = true;
+console.log(pass);
+
+var number = 55.50;
+console.log(typeof number);
+
+var num = "55.50";
+console.log(typeof num);
