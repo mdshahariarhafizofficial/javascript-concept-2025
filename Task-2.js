d04a90e8157070b86211ae5d0d54141910705179
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-02-05 19:40:35 +0600

    Task-2 Complete

---
+++ Task-2.js
@@ -0,0 +1,29 @@
+// All Subject Marks
+console.log("All Subject Marks");
+console.log(" ");
+
+var Mathematics = 75.25;
+console.log("Mathematics     - " + Mathematics);
+
+var Biology = 65;
+console.log("Biology         - " + Biology);
+
+var Chemistry = 80;
+console.log("Chemistry       - " + Chemistry);
+
+var Physics = 35.45;
+console.log("Physics         - " + Physics);
+
+var Bangla = 99.50;
+console.log("Bangla          - " + Bangla);
+
+console.log("------------------------");
+
+// Total Mark
+var total = Mathematics+Biology+ Bangla+ Chemistry+ Physics;
+console.log("Total Marks     = " + total);
+
+var avg = total/5;
+console.log("Average marks   = " + avg.toFixed(2));
+
+
