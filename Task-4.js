3b5e9e507b11af74c9e010de0bd4b6cc1b156c5b
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-02-05 20:03:56 +0600

    Task-4 Complete

---
+++ Task-4.js
@@ -0,0 +1,18 @@
+var a = isNaN('11');
+var b = isNaN(2-10);
+var c = isNaN(8);
+var d = isNaN("hello");
+
+// isNaN means "Not a Number" If the isNaN value is a Number the return is false. and if the isNaN value is not a number then they return true.
+
+// a - false
+// b - false
+// c - false
+// d - true
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
