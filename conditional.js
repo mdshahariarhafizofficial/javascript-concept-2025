63e58e9f452b11bbb274b766fc3b055ba2580579
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-02-06 22:01:12 +0600

    complex condition practice done

---
+++ conditional.js
@@ -0,0 +1,22 @@
+var a = 50;
+var b = 100;
+
+if( a > b ){
+    console.log("a is bigger");
+}
+else{
+    console.log("b is bigger");
+    
+}
+
+var salary = 50000;
+var Bcs = false;
+var age = 22;
+
+if( (salary > 25000 && age >= 21) || (Bcs === true) ){
+    console.log("You are  ready for marriage");
+}
+else{
+    console.log("You are not ready for marriage");
+    
+}
