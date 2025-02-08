cd2958da33473494ff72d2c32026a2d024f3b9e1
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-02-08 19:21:32 +0600

    shorthand condition practice done

---
+++ shorthand.js
@@ -0,0 +1,9 @@
+var salary = 1000;
+
+salary > 10000 ? console.log("Your application approved") : console.log("Your application rejected");
+ ;
+
+ var time = 21;
+
+ time < 18 ? console.log("Good Day!") : console.log("Good Evening!");
+ 
