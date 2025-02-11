52e9517b85fddda40f1931d105024feda483b991
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-02-11 19:03:33 +0600

    Task-1 Complete

---
+++ Array_Task/Task-1.js
@@ -0,0 +1,11 @@
+// 1. Declare an array with 5 elements containing fruits
+const fruits = ["Apple", "Banana", "Orange", "Mango", "Lemon"];
+
+// 2. console log the 3rd index element
+console.log(fruits[3]);
+
+// 3. change the value of the 2nd index element to jambura
+fruits[2] = "jambura";
+
+// 4. console log the final array
+console.log(fruits);
