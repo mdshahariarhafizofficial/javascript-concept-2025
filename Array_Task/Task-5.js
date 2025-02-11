38ca21754dda4145d77b09cdf90bc4b06da9516e
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-02-11 19:45:56 +0600

    Task-5 Complete

---
+++ Array_Task/Task-5.js
@@ -0,0 +1,17 @@
+// Create two arrays of your choice.
+const marks = [45, 35, 78];
+const names = ["Rohim", "Korim", "Jasim"]
+
+// Use the concat method to combine the two arrays into a new array.
+let result = names.concat(marks);
+
+// Print both the original arrays and the combined array using console.log().
+console.log(marks);
+console.log(names);
+console.log(result);
+
+let h = "Hello";
+let w = "World"
+let output = h.concat(" ", w)
+console.log(output);
+
