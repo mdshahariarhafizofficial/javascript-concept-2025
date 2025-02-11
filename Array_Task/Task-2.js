25182c2ad32b1bf7cd10b5f13c451461c4b9ba03
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-02-11 19:11:24 +0600

    Task-2 Complete

---
+++ Array_Task/Task-2.js
@@ -0,0 +1,18 @@
+// Declare an array of 3 tourist destinations
+const touristDestination = ["Kuwakata", "Coxs Bazar", "Saintmartin"];
+console.log(touristDestination);
+
+// Add a new tourist destination to your tourist array
+touristDestination.push("Sajek Velly");
+console.log(touristDestination);
+
+// Add two more to your array
+touristDestination.push("Bandarban", "Tetulia");
+console.log(touristDestination);
+
+// Remove the last tourist destination you have added
+touristDestination.pop()
+
+// display the final array as output
+console.log(touristDestination);
+
