e47036b8043042c385f97bf593ee00184a0d3074
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-02-13 22:54:51 +0600

    Task-4 complete

---
+++ For Loop Task/4. multiplication table.js
@@ -0,0 +1,9 @@
+// Generate a multiplication table for number 9
+console.log("Multiplication table for number 9 :");
+
+
+let number = 9;
+for( let i = 1; i <=10; i++ ){
+    let mulTable = number*i;
+    console.log(number, "X", i, " = ",mulTable); 
+}
