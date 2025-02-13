ad408bf6474b36163ce661fd3003a7b34e0dc14f
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-02-13 22:29:52 +0600

    Tak-2 Complete

---
+++ For Loop Task/2. odd even.js
@@ -0,0 +1,22 @@
+// Odd Number
+console.log();
+console.log("Odd Number");
+
+for( let i = 61; i <=100; i++ ){
+    if( i % 2 !== 0 ){
+        console.log(i, "- is odd number");
+        
+    }   
+}
+
+
+// Even Number
+console.log();
+console.log("Even Number");
+
+for( let i = 78; i <= 98; i++ ){
+    if( i%2 ===0 ){
+        console.log(i, "- is Even Number");
+        
+    }
+}
