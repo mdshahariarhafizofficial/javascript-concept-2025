bbf3791d0164fd0fc69658872df83b1751ca500a
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-02-13 20:41:19 +0600

    While Loop practice done

---
+++ for_loops.js
@@ -0,0 +1,11 @@
+let number = [2, 54, 45, 65, 85, 74, 232, 200];
+
+for( let i of number ){
+    console.log(i);
+    
+}
+
+let fruits = ["Banana", "Mango", "Apple", "Lemon"];
+for( let x of fruits ){
+    console.log(x);
+}
---
+++ while.js
@@ -0,0 +1,19 @@
+let i = 1;
+while( i <= 10){
+    console.log(i);
+    i++;
+}
+
+let num = 65;
+while( num <= 100 ){
+    console.log(num);
+    num++;
+}
+
+let n = 1;
+let sum = 0;
+while( n <= 10 ){
+    sum = sum+n;
+    n++;
+};
+console.log("Sum = ", sum); 
