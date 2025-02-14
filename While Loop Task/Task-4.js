6f49ab5080f065a25438a2cfa8542d61130a8647
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-02-14 19:35:17 +0600

    Task-4 Complete

---
+++ While Loop Task/Task-4.js
@@ -0,0 +1,9 @@
+// As Era is learning now, she wants to explore more and more. Tell Era to generate a multiplication table for number 5
+
+let number = 5;
+let i = 1;
+while( i <= 10 ){
+    let mulTable = number*i;
+    console.log(number, " X ", i, "=" ,mulTable);
+    i++;
+}
