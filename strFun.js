2e37c8952d3a9684becbdcf73ed27b7ad593abcd
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-02-15 14:22:18 +0600

    strFun Done

---
+++ strFun.js
@@ -0,0 +1,19 @@
+let name = new String("Hlw World");
+console.log(name);
+
+let country = "bangladesh";
+let fevFood = "FISH";
+let pra = " I love my Country ";
+
+console.log(country.toUpperCase());
+console.log(fevFood.toLocaleLowerCase());
+console.log(pra.trim());
+
+// Slice
+let text = "My Favorite Player is Fahad Ahmad";
+console.log(text.slice(3, 11));
+console.log(text.slice(19, 21));
+
+// Split
+console.log(text.split(" "));
+
